1bba13c512debf4a222518a127ca82b2f3290358
csharkiscript <47679352+csharkiscript@users.noreply.github.com>
2019-06-08 20:31:47 -0300

    Add files via upload

--- Sangralaxy/Game.py
+++ Sangralaxy/Game.py
@@ -1,4 +1,5 @@
 import random
+import _thread
 def echo(text):
     print(text)
 
@@ -53,8 +54,8 @@ def f(text1, text2, text3, text4, text5):
 x = 0
 y = 0
 life = 1000 
-x1 = 0
-y1 = 0
+x1 = 25
+y1 = 30
 damagep = 20
 coin = 0
 
@@ -114,13 +115,13 @@ def area(tamx, tamy, keyxm, keyxn, keyym, keyyn, listo):
                             print("+", vC,"COINS")
 
                 if typ1 == 'chestlife':
+                        if c == 'YES':
+                            vL = random.randint(10,1000)
+                            life += vL
                     if ch == cd:
                         
                         y = y2 - 1
                         c = input('Open chest?')
-                        if c == 'YES':
-                            vL = random.randint(10,1000)
-                            life += vL
                             print("+", vL,"LIFE")
 
                 che += 3 
@@ -165,126 +166,137 @@ def bossarea(bossname, damage, lifeb, tamx, tamy, keyxn, keyyn, keyym, keyxm, li
     
 
     respostapoint = ''
+
+       
     while respostapoint != 0:
-        f = random.choice(listf)
-        cp = (x, y)
-        cb = (x1, y1)
-        
-        print("""     
-            {}
-        {}      {}
-            {}  f""".format(keyxn, keyyn, keyym, keyxm))
-        respostapoint = input('')
-        if respostapoint == keyxn:
-            x -= 1
-        if respostapoint == keyxm:
-            x += 1
-        if respostapoint == keyyn:
-            y -= 1
-        if respostapoint == keyym:
-            y += 1
-        if respostapoint == 'f':
-            if y == y1:
-                if x == x1:
-                    print('You attacked')
-                    lifeb = lifeb - damagep
-                    y1 += 1
-                    damagec = 0
-        if x >= tamx:
-            x = tamx
-        if y >= tamy:
-            y = tamy
-        if x <= 0:
-            x = 0
-        if y <= 0:
-            y = 0
+            global life
+
+            global x 
+            global y
+            global x1
+            global y1
+            global damagep
+            
+            cp = (x, y)
+            cb = (x1, y1)
+                
+            print("""     
+                {}
+            {}      {}
+                {}  f""".format(keyxn, keyyn, keyym, keyxm))
+            respostapoint = input('')
+            if respostapoint == keyxn:
+                x -= 1
+            if respostapoint == keyxm:
+                x += 1
+            if respostapoint == keyyn:
+                y -= 1
+            if respostapoint == keyym:
+                y += 1
+            if respostapoint == 'f':
+                if y == y1:
+                    if x == x1:
+                        print('You attacked')
+                        lifeb = lifeb - damagep
+                        y1 += 1
+                        damagec = 0
+            if x >= tamx:
+                x = tamx
+            if y >= tamy:
+                y = tamy
+            if x <= 0:
+                x = 0
+            if y <= 0:
+                y = 0
 
         
+            
+            f = random.choice(listf)
+            if damagec == 0:
+                while superc >= 5:
+                    czx = 0 + x
+                    czy = 0 + y
+                    ctx = tamx - x
+                    cty = tamy - y
+                    for c in range(0, 6):
+                        if x1 == tamx:
+                            y1 -= 1
+                        elif y1 == tamy:
+                            x1 -= 1
+                        elif x1 == 0:
+                            y1 += 1
+                        elif y1 == 0:
+                            x1 += 1
+                        elif xc <= yc:
+                            y1 += 1
+                        elif yc <= xc:
+                            x1 += 1
+                        else:
+                            if x <= y:
+                                if czy <= cty:
+                                    y += 1
+                                if czy >= cty:
+                                    y -= 1
+                            if y <= x:
+                                if czx <= ctx:
+                                    x += 1
+                                if czx >= ctx:
+                                    x -= 1
+                    superc = 0
+                damagec = 1
+                    
+                    
+                            
+                        
 
-        if damagec == 0:
-            while superc >= 5:
-                czx = 0 + x
-                czy = 0 + y
-                ctx = tamx - x
-                cty = tamy - y
-                for c in range(0, 6):
-                    if x1 == tamx:
-                        y1 -= 1
-                    elif y1 == tamy:
+
+            if damagec == 1:
+
+                xc = x - x1
+                yc = y - y1
+                if xc <= yc:
+                    if x <= x1:
                         x1 -= 1
-                    elif x1 == 0:
-                        y1 += 1
-                    elif y1 == 0:
+                    if x >= x1:
                         x1 += 1
-                    elif xc <= yc:
-                        y1 += 1
-                    elif yc <= xc:
-                        x1 += 1
-                    else:
-                        if x <= y:
-                            if czy <= cty:
-                                y += 1
-                            if czy >= cty:
-                                y -= 1
-                        if y <= x:
-                            if czx <= ctx:
-                                x += 1
-                            if czx >= ctx:
-                                x -= 1
-                superc = 0
-            damagec = 1
 
-            
-            
+                if xc >= yc:
+                    if y <= y1:
+                        y1 -= 1
+                    if y >= y1:
+                        y1 += 1
+                    
+                    
+                if cb == cp:
+                    print(f)
+                    life = life - damage
+                    print(bossname, 'attacked')
+                    superc += 1
+
                     
                 
+            if x1 >= tamx:
+                x1 = tamx
+            if y1 >= tamy:
+                y1 = tamy
+            if x1 <= 0:
+                x1 = 0
+            if y1 <= 0:
+                y1 = 0
 
-
-        if damagec == 1:
-
-            xc = x - x1
-            yc = y - y1
-            if xc <= yc:
-                if x <= x1:
-                    x1 -= 1
-                if x >= x1:
-                    x1 += 1
-
-            if xc >= yc:
-                if y <= y1:
-                    y1 -= 1
-                if y >= y1:
-                    y1 += 1
-            
-            
-            if cb == cp:
-                print(f)
-                life = life - damage
-                print(bossname, 'attacked')
-                superc += 1
-
-            
         
-        if x1 >= tamx:
-            x1 = tamx
-        if y1 >= tamy:
-            y1 = tamy
-        if x1 <= 0:
-            x1 = 0
-        if y1 <= 0:
-            y1 = 0
+                    
+            print('YOU=', x, y, life)
         
-        print('YOU=', x, y, life)
-     
-        print('BOSSc', x1, y1, lifeb, superc)
+            print('BOSS=', x1, y1, lifeb, superc)
 
-        if life <= 0:
-            print('You died')
-            break
+            if life <= 0:
+                print('You died')
+                break
 
-        if lifeb <= 0:
-            print(bossname, 'died')
-            break 
+            if lifeb <= 0:
+                print(bossname, 'died')
+                break 
 
 def menus():
     menu = input("""
@@ -293,10 +305,11 @@ def menus():
     
     
     """)
-    if menu == '1'
+    if menu == '1':
         print('')
-    elif menu == '2'
+    elif menu == '2':
         exit()
     
 
 
+                                                                           
